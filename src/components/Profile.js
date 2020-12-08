f7ac1f91d92190b7899aef3521c2c6e6a3f220f8
Keith Fung <fungsenkeith@gmail.com>
2020-12-08 09:04:20 -0500

    profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,30 +1,33 @@
 import React from "react";
+import notetaker from "../images/notetaker.png";
+import weather from "../images/weather.png";
+import team from "../images/team.png";
+import budget from "../images/budget.png";
 function Profile() {
   const projects = [
     {
       name: "Progressive Budget",
-      image: "",
+      image: budget,
       description:
         "Helps you keep track of your budget in a fun and visual way",
       url: "https://github.com/ikeifu/progressive-budget",
     },
     {
-      name: "Employee Management",
-      image: "",
+      name: "Team Manager",
+      image: team,
       description:
         "Can set up and manage your employees to see who is in which department and what they do, can also add new departments.",
-      url: "https://github.com/ikeifu/employee-management",
+      url: "https://github.com/ikeifu/team-profile-generator",
     },
     {
-      name: "Eat Da Burger",
-      image: "",
-      description:
-        "Dynamically keeping track of what burgers you've had and what burgers you want to eat",
-      url: "https://eat-da-burger-1088.herokuapp.com/",
+      name: "Weather Dashboard",
+      image: weather,
+      description: "Can track the weather",
+      url: "https://github.com/ikeifu/Weather-DashBoard",
     },
     {
       name: "Note taker",
-      image: "",
+      image: notetaker,
       description: "Helps you keep track of your notes",
       url: "https://github.com/ikeifu/note-taker",
     },
@@ -32,18 +35,20 @@ function Profile() {
   return (
     <div className="row">
       {projects.map((project, i) => {
-        <div className="col-lg-6 mb-3" key={i}>
-          <img
-            src={project.image}
-            alt={project.name}
-            className="portfolio-image"
-          />
-          <h4>{project.name}</h4>
-          <a href={project.url} target="_blank" rel="noreferrer">
-            Link to github repo
-          </a>
-          <p>{project.description}</p>
-        </div>;
+        return (
+          <div className="col-lg-6 mb-3" key={i}>
+            <img
+              src={project.image}
+              alt={project.name}
+              className="portfolio-image"
+            />
+            <h4>{project.name}</h4>
+            <a href={project.url} target="_blank" rel="noreferrer">
+              Link to github repo
+            </a>
+            <p>{project.description}</p>
+          </div>
+        );
       })}
     </div>
   );
